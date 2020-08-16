0a2803d78624e00f8f8af9e9570fb1ad4a38858c
Andreas Odin Kammerloher <kammerloher.andreas@gmail.com>
2020-08-16 20:58:25 +0200

    Count obligations

--- lib/mix/tasks/tldrlixir.ex
+++ lib/mix/tasks/tldrlixir.ex
@@ -9,40 +9,38 @@ defmodule Mix.Tasks.Tldrlixir do
     # csv: :boolean
   ]
 
+  @spec run([binary]) :: any
   def run(argv) do
     {opts, _argv} = OptionParser.parse!(argv, switches: @switches)
 
     Licensir.Scanner.scan(opts)
     |> Enum.map(&extract_name_and_license/1)
+    |> Enum.map(fn %{name: name, license: license} -> {name, license, to_obligations(license)} end)
+    |> add_obligation_counts()
     |> IO.inspect()
-
-    # |> Enum.sort_by(fn license -> license.name end)
-    # |> Enum.map(&to_row/1)
-    # |> render(opts)
   end
 
   @spec extract_name_and_license(License.t()) :: name_and_license
   defp extract_name_and_license(%{name: name, license: license}),
     do: %{name: name, license: license}
 
-  def main(args \\ []) do
-    args
-    |> parse_args()
-    |> response()
-    |> IO.puts()
+  @spec to_obligations(String.t()) :: String.t()
+  defp to_obligations(license_name) do
+    obligations_by_license = Tldrlixir.LicenseData.obligations_by_license()
 
-    Licensir.Scanner.scan([])
+    Map.get(obligations_by_license, license_name, ["uncertain license - not sure what to do"])
   end
 
-  defp parse_args(args) do
-    {opts, text, _} =
-      args
-      |> OptionParser.parse(switches: [reverse: :boolean])
+  @spec add_obligation_counts(any) :: %{
+          names_and_licenses: list(name_and_license),
+          obligations: map()
+        }
+  defp add_obligation_counts(names_and_licenses) do
+    obligations =
+      names_and_licenses
+      |> Enum.flat_map(fn {_, _, obligations} -> obligations end)
+      |> Enum.reduce(%{}, fn obligation, acc -> Map.update(acc, obligation, 1, &(&1 + 1)) end)
 
-    {opts, List.to_string(text)}
-  end
-
-  defp response({opts, text}) do
-    if opts[:reverse], do: String.reverse(text), else: text
+    %{names_and_licenses: names_and_licenses, obligations: obligations}
   end
 end
